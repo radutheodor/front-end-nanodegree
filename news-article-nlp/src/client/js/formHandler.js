d9de2def2f574f0d7283a4c1165e5a90e5dc3e67
rth <theodor.radu@avaloq.com>
2020-03-13 13:56:46 +0100

    Apply all working changes from Build Tools lesson

--- news-article-nlp/src/client/js/formHandler.js
+++ news-article-nlp/src/client/js/formHandler.js
@@ -1,16 +1,17 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+  // check what text was put into the form field
+  console.log("Moe");
+  let formText = document.getElementById("name").value;
+  Client.checkForName(formText);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+  console.log("::: Form Submitted :::");
+  fetch("http://localhost:8082/test")
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+      document.getElementById("results").innerHTML = res.message;
+    });
 }
 
-export { handleSubmit }
+export { handleSubmit };
--- news-article-nlp/src/server/index.js
+++ news-article-nlp/src/server/index.js
@@ -1,23 +1,29 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express()
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-app.use(express.static('dist'))
+app.use(express.static(path.join("../..", "dist")));
 
-console.log(__dirname)
-
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+app.get("/", function(req, res) {
+  res.sendFile("index.html", { root: "../../dist" });
+});
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+app.listen(8082, function() {
+  console.log("Example app listening on port 8082!");
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function(req, res) {
+  res.send(mockAPIResponse);
+});
